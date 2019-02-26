9f4246415cca921f6b6727e1a8668f478e250f8d
Yeshi <tyeshi777@yahoo.com>
2019-02-26 11:54:16 -0800

    completed closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,9 +1,15 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-
+function outer() {
+  const data = "closures are ";
+  return function inner() {
+    const innerData = "awesome";
+    console.log(data + innerData);
+  };
+}
+let execute = outer();
+execute();
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
